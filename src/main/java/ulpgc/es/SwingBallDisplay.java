24a9e5ae223e5634acab74d7827a77e781f147c7
edulanarf <edulanarfrgmail.com>
2025-01-17 13:32:12 +0000

    Se añaden sincronizaciones en los metodos paint, drawCircle y clear para evitar que la lista paints no se actualize por 2 tareas diferentes

--- src/main/java/ulpgc/es/SwingBallDisplay.java
+++ src/main/java/ulpgc/es/SwingBallDisplay.java
@@ -9,25 +9,31 @@ public class SwingBallDisplay extends JPanel implements BallDisplay {
     private List<Paint> paints = new ArrayList<>();
     @Override
     public void drawCircle(int r, int y) {
-        paints.add(new Paint(y,r));
-        repaint();
+        synchronized (paints) {
+            paints.add(new Paint(y, r)); // Agregar de manera segura
+        }
+        repaint(); // Forzar la actualización del panel
     }
 
     @Override
     public void clear() {
-        paints.clear();
+        synchronized (paints) {
+            paints.clear();
+        }
         repaint();
     }
 
     @Override
-    public void paint(Graphics g){
-        for(Paint paint: paints){
-            int centerX = (this.getWidth() / 2) - paint.r(); // Centrar en X
-            int centerY = paint.y(); // Coordenada Y del círculo
-            int diameter = paint.r() * 2; // Diámetro del círculo
+    public void paint(Graphics g) {
+        synchronized (paints) {
+            for (Paint paint : paints) {
+                int centerX = (this.getWidth() / 2) - paint.r(); // Centrar en X
+                int centerY = paint.y(); // Coordenada Y del círculo
+                int diameter = paint.r() * 2; // Diámetro del círculo
 
-            // Dibujar el contorno del círculo
-            g.drawOval(centerX, centerY, diameter, diameter);
+                // Dibujar el contorno del círculo
+                g.drawOval(centerX, centerY, diameter, diameter);
+            }
         }
     }
 
